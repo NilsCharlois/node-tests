f6e69fc4dd1456b9edc8375af3836de015711a3e
nilscharlois <nils.charlois.charlois@gmail.com>
2019-02-21 23:34:22 +0000

    Lecture 57 done

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 
-var app  = express;
+var app = express();
 
 app.get('/', (req, res)=>{
-  res.send('hello world!');
-})
+  res.status(404).send({
+    error:'Page not found',
+    name:'Todo App v1.0'
+  });
+});
 
-app.listen(3000)
+app.get('/users', (req, res)=>{
+  res.status(200).send([{
+    name:'Nils',
+    age:31
+  },{
+    name:'Test',
+    age:2
+  }]);
+});
+
+
+
+
+app.listen(3000);
+
+module.exports.app = app;
